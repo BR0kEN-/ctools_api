fb0a5b20b4d14b4f9724cb394664f0770b1423e2
Sergii Bondarenko <broken@firstvector.org>
2018-07-13 21:45:40 +0300

    Test

--- src/Tests/PhpUnit/CommonTest.php
+++ src/Tests/PhpUnit/CommonTest.php
@@ -2,15 +2,28 @@
 
 namespace Drupal\Tests\ctools_api\PhpUnit;
 
+/**
+ * The set of tests of common module functionality.
+ */
 class CommonTest extends \PHPUnit_Framework_TestCase {
 
   /**
+   * Tests transformation of strings to the "underscore" format.
+   *
+   * @param string $input
+   *   The initial value to transform.
+   * @param string $expected
+   *   The value that's expected after processing.
+   *
    * @dataProvider providerToUnderscore
    */
-  public function testToUnderscore($value, $expected) {
-    static::assertSame($expected, ctools_api_to_underscore($value));
+  public function testToUnderscore($input, $expected) {
+    static::assertSame($expected, ctools_api_to_underscore($input));
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function providerToUnderscore() {
     return [
       ['MyCamelCaseString', 'my_camel_case_string'],
