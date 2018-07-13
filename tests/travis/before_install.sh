cc6f70d0cf185285924fd3ed548a4f1c8b774a98
Sergii Bondarenko <broken@firstvector.org>
2018-07-13 18:27:03 +0300

    Test

--- tests/travis/before_install.sh
+++ tests/travis/before_install.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-set -e
-
 # Go to the directory where this script is located.
 cd "$(dirname "$0")"
 
-# Inject custom PHP configuration.
+echo "Inject custom PHP configuration."
 phpenv config-add php/php.ini
-# Install dependencies.
+
+echo "Install dependencies."
 composer install
 
+echo "Compute facts."
 export CWD="$(pwd -P)"
 # Make our packages observable.
 export PATH="$CWD/bin:$PATH"
@@ -18,14 +18,18 @@ export TRAVIS_BUILD_DIR_OLD="$TRAVIS_BUILD_DIR"
 # Override the path to directory with a build.
 export TRAVIS_BUILD_DIR="$CWD/$DRUPAL_TEST_MODULE_LOCATION/$DRUPAL_TEST_MODULE_NAME"
 
-# Ensure the directory exists.
+echo "Ensure the directory for project exists."
 mkdir -p "$TRAVIS_BUILD_DIR"
-# Copy the module.
+
+echo "Copy the project."
 rsync -ra --delete --exclude="${CWD//$TRAVIS_BUILD_DIR_OLD\//}" "$TRAVIS_BUILD_DIR_OLD" "$TRAVIS_BUILD_DIR"
 
-# Discover available coding standards.
+echo "Discover available coding standards."
 phpcs --config-set installed_paths "$(find "$CWD/vendor/" -type f -name "ruleset.xml" -exec dirname {} \; | xargs dirname | uniq | paste -sd "," -)"
+
+echo "Rehash PHP environment."
 phpenv rehash
 
+echo "Configure Apache web-server."
 sudo a2enmod rewrite actions fastcgi alias
 sudo service apache2 restart
