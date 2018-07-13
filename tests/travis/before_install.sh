6d4c54f8692b4c9e85f0f2f211e8a9c8b76d5903
Sergii Bondarenko <broken@firstvector.org>
2018-07-13 16:01:15 +0300

    Test

--- tests/travis/before_install.sh
+++ tests/travis/before_install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
+# Fail as early as possible.
 set -e
-shopt -s extglob
-
+# Go to the directory where this script is located.
 cd "$(dirname "$0")"
 
 # Inject custom PHP configuration.
@@ -18,10 +18,13 @@ export TRAVIS_BUILD_DIR_OLD="$TRAVIS_BUILD_DIR"
 # Override the path to directory with a build.
 export TRAVIS_BUILD_DIR="$DRUPAL_TEST_MODULE_LOCATION/$DRUPAL_TEST_MODULE_NAME"
 
+# @todo
+echo "$CWD"
+
 # Ensure the directory exists.
 mkdir -p "$TRAVIS_BUILD_DIR"
 # Copy the module.
-rsync -ra --delete --exclude="$CWD" "$TRAVIS_BUILD_DIR_OLD" "$TRAVIS_BUILD_DIR"
+rsync -ra --delete --exclude="tests" "$TRAVIS_BUILD_DIR_OLD" "$TRAVIS_BUILD_DIR"
 
 # Discover available coding standards.
 phpcs --config-set installed_paths "$(find "$CWD/vendor/" -type f -name "ruleset.xml" -exec dirname {} \; | xargs dirname | uniq | paste -sd "," -)"
