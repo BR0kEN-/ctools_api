aca206d2644bd9be1bd3a60a5cea2d8d7bcd3e17
Sergii Bondarenko <broken@firstvector.org>
2018-07-13 19:22:17 +0300

    Test

---
+++ tests/travis/bootstrap.phpunit.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Bootstrap Drupal to run PHPUnit tests.
+ */
+
+// @todo Read "drupal" from "composer.json" or "DRUPAL_TEST_SUBDIR" variable.
+define('DRUPAL_ROOT', getcwd() . '/drupal');
+
+$_SERVER += [
+  // Prevent notices.
+  'REMOTE_ADDR' => '127.0.0.1',
+];
+
+require_once DRUPAL_ROOT . '/../vendor/autoload.php';
+require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
