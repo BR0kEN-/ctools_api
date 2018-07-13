a36ab2a0ebd96453b4a5cff1b0b0bcfe5738484c
Sergii Bondarenko <broken@firstvector.org>
2018-07-13 15:52:14 +0300

    Test

--- tests/travis/before_install.sh
+++ tests/travis/before_install.sh
@@ -10,19 +10,20 @@ phpenv config-add php/php.ini
 # Install dependencies.
 composer install
 
+export CWD="$(pwd -P)"
+# Make our packages observable.
+export PATH="$CWD/bin:$PATH"
 # Save the old location.
 export TRAVIS_BUILD_DIR_OLD="$TRAVIS_BUILD_DIR"
-# Update the PATH before the "TRAVIS_BUILD_DIR" gets changed.
-export PATH="$(pwd -P)/bin:$PATH"
 # Override the path to directory with a build.
 export TRAVIS_BUILD_DIR="$DRUPAL_TEST_MODULE_LOCATION/$DRUPAL_TEST_MODULE_NAME"
 
 # Ensure the directory exists.
 mkdir -p "$TRAVIS_BUILD_DIR"
 # Copy the module.
-rsync -ra --delete --exclude="$(dirname "$0")" "$TRAVIS_BUILD_DIR_OLD" "$TRAVIS_BUILD_DIR"
+rsync -ra --delete --exclude="$CWD" "$TRAVIS_BUILD_DIR_OLD" "$TRAVIS_BUILD_DIR"
 
 # Discover available coding standards.
-phpcs --config-set installed_paths "$(find vendor/ -type f -name "ruleset.xml" -exec dirname {} \; | xargs dirname | uniq | paste -sd "," -)"
+phpcs --config-set installed_paths "$(find "$CWD/vendor/" -type f -name "ruleset.xml" -exec dirname {} \; | xargs dirname | uniq | paste -sd "," -)"
 a2enmod rewrite actions fastcgi alias
 phpenv rehash
